ad1044b7e16891f1d36b265baf1d761db8bdef53
varun-tokas <106471691+varun-tokas@users.noreply.github.com>
2022-08-16 19:07:04 +0530

    Update sol.cpp

--- sol.cpp
+++ sol.cpp
@@ -3,9 +3,12 @@
 using namespace std;
 
 signed main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
   int n,m;
   cin>>n>>m;
-  vi tr((1<<(n+1)),0);
+  vector<int> tr((1<<(n+1)),0);
   for(int i=0;i<1<<n;i++) {
     cin>>tr[i|(1<<n)];
   }
